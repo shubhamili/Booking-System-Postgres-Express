63d5b822e7b3eb0cc6864236eb9983319a4eccc0
shubham singhk <shubhamsinghia160@gmail.com>
2025-10-14 22:24:45 +0530

    save changes

---
+++ server/src/controllers/booking.controller.ts
@@ -0,0 +1,17 @@
+import type { Request, Response } from "express";
+
+const createBooking = async (req: Request, res: Response) => {
+    try {
+
+        const {
+            showId,
+            seatId,
+        } = req.body;
+
+        
+
+
+    } catch (error) {
+
+    }
+}
