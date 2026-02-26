2c1e022dd502d53641d1021cbc7bf760dad065d7
shubham singhk <shubhamsinghia160@gmail.com>
2026-02-26 16:19:29 +0530

    form style changes

--- client/src/pages/admin/movie/AddEditMovie.tsx
+++ client/src/pages/admin/movie/AddEditMovie.tsx
@@ -3,6 +3,7 @@ import { useForm } from 'react-hook-form'
 import z from 'zod'
 import { Input } from '../../../components/ui/input'
 import { Label } from '../../../components/ui/label'
+import { Button } from '../../../components/ui/button'
 
 
 export const movieSchema = z.object({
@@ -66,11 +67,11 @@ export const defaultMovieValues = {
     poster: undefined,
 }
 
-type MovieFormProps = {
-    initialData?: MovieFormValues
-    onSubmit?: (data: MovieFormValues) => void
-    isLoading: boolean
-}
+// type MovieFormProps = {
+//     initialData?: MovieFormValues
+//     onSubmit?: (data: MovieFormValues) => void
+//     isLoading: boolean
+// }
 
 const AddEditMovie = () => {
     const {
@@ -100,13 +101,14 @@ const AddEditMovie = () => {
     console.log(values)
     return (
         <>
+            <h2 className='text-xl'>Add Movie</h2>
 
             <form
-                className='flex flex-col gap-2 '
+                className='flex flex-col gap-4 m-5'
                 onSubmit={handleSubmit(onSubmit)}>
 
                 <div>
-                    <Label htmlFor="">Title</Label>
+                    <Label className='m-1' htmlFor="">Title</Label>
                     <Input
                         type="text"
                         {...register("title")}
@@ -114,14 +116,15 @@ const AddEditMovie = () => {
                     {errors.title && <p className="text-red-400 text-sm mt-1">{errors.title.message}</p>}
                 </div>
                 <div>
-                    <Label htmlFor="">description</Label>
+                    <Label className='m-1' htmlFor="">Description</Label>
                     <textarea
+                        className='w-full p-2'
                         {...register("description")}
                     ></textarea>
                     {errors.description && <p className="text-red-400 text-sm mt-1">{errors.description.message}</p>}
                 </div>
                 <div>
-                    <Label htmlFor="duration">Duration</Label>
+                    <Label className='m-1' htmlFor="duration">Duration</Label>
                     <Input
                         type="number"
                         {...register("duration", { valueAsNumber: true })}
@@ -129,47 +132,47 @@ const AddEditMovie = () => {
                     {errors.duration && <p className="text-red-400 text-sm mt-1">{errors.duration.message}</p>}
                 </div>
                 <div>
-                    <Label htmlFor="Type">Type</Label>
+                    <Label className='m-1' htmlFor="Type">Type</Label>
                     <Input type="text"
                         {...register("type")} />
                     {errors.type && <p className="text-red-400 text-sm mt-1">{errors.type.message}</p>}
                 </div>
                 <div>
-                    <Label htmlFor="language">language</Label>
+                    <Label className='m-1' htmlFor="language">Language</Label>
                     <Input type="text"
                         {...register("language")} />
                     {errors.language && <p className="text-red-400 text-sm mt-1">{errors.language.message}</p>}
                 </div>
                 <div>
-                    <Label htmlFor="genre">genre</Label>
+                    <Label className='m-1' htmlFor="genre">Genre</Label>
                     <Input type="text"
                         {...register("genre")} />
                     {errors.genre && <p className="text-red-400 text-sm mt-1">{errors.genre.message}</p>}
                 </div>
                 <div>
-                    <Label htmlFor="rating">rating</Label>
+                    <Label className='m-1' htmlFor="rating">Rating</Label>
                     <Input type="number"
                         {...register("rating", { valueAsNumber: true })} />
                     {errors.rating && <p className="text-red-400 text-sm mt-1"> {errors.rating.message}</p>}
                 </div>
                 <div>
-                    <Label htmlFor="trailerUrl">trailerUrl</Label>
+                    <Label className='m-1' htmlFor="trailerUrl">Trailer Url</Label>
                     <Input type="text"
                         {...register("trailerUrl")} />
                     {errors.trailerUrl && <p className="text-red-400 text-sm mt-1">{errors.trailerUrl.message}</p>}
                 </div>
                 <div>
-                    <Label htmlFor="releaseDate">releaseDate</Label>
+                    <Label className='m-1' htmlFor="releaseDate">Release Date</Label>
                     <Input type="date" {...register("releaseDate")} />
                     {errors.releaseDate && <p className="text-red-400 text-sm mt-1">{errors.releaseDate.message}</p>}
                 </div>
                 <div>
-                    <Label htmlFor="currency">currency</Label>
+                    <Label className='m-1' htmlFor="currency">Currency</Label>
                     <Input type="text" {...register("currency")} />
                     {errors.currency && <p className="text-red-400 text-sm mt-1">{errors.currency.message}</p>}
                 </div>
                 <div>
-                    <Label htmlFor="poster">poster</Label>
+                    <Label className='m-1' htmlFor="poster">Poster</Label>
                     <Input
                         type="file"
                         onChange={(e) => {
@@ -179,19 +182,11 @@ const AddEditMovie = () => {
                     {errors.poster && <p className="text-red-400 text-sm mt-1">{errors.poster.message}</p>}
                 </div>
 
-                <button type="submit">Submit</button>
+                <Button type="submit">Submit</Button>
             </form>
-
-
 
         </>
     )
-
-
-
-
-
-
 
 }
 
